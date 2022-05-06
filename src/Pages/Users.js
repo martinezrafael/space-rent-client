084d6794f4bd31d7998a4b04c95a79bc7b9415cd
Rafael Molina Martinez <rafaelmolinamartinez@pop-os.localdomain>
2022-05-06 12:55:35 -0300

    front users page

--- src/Pages/Users.js
+++ src/Pages/Users.js
@@ -2,35 +2,44 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserList from "../components/UserList";
 import apiUtils from "../utils/api.utils";
+import styled from "styled-components";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const UsersContainer = styled.div`
+  max-width: 80%;
+  margin: auto;
+  display: flex;
+`;
 
 const Users = () => {
-    const [ users, setUsers ] = useState([]);
+    const [users, setUsers] = useState([]);
 
     const navigate = useNavigate();
 
     const getAllUsers = async () => {
         try {
             const users = await apiUtils.getUsers();
-            setUsers(users)
+            setUsers(users);
         } catch (error) {
             console.error(error);
-            navigate('/login');
+            navigate("/login");
         }
-    }
+    };
 
     useEffect(() => {
         getAllUsers();
-    }, [])
+    }, []);
 
     return (
         <div>
-            
-            <input id="searchUser" />
-            <UserList users={users} getAllUsers={getAllUsers}/>
-            <h2>teste</h2>
+            <Header />
+            <UsersContainer>
+                <UserList users={users} getAllUsers={getAllUsers} />
+            </UsersContainer>
+            <Footer />
         </div>
-    )
-}
-
+    );
+};
 
 export default Users;
